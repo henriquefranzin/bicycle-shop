7c12c0fd607a622359c77cd5d826340f11ee88cf
Henrique Franzin <henriquefranzin@gmail.com>
2021-09-17 16:05:03 -0300

    feat: add product filters

--- src/js/controllers/Products.js
+++ src/js/controllers/Products.js
@@ -1,20 +1,67 @@
 import ProductsView from '../views/Products.js';
+import ProductsModel from '../models/Products.js';
 import { getProducts } from '../services/products.js';
 
 export default class ProductsController {
   constructor() {
     this.productsView = new ProductsView('#product-list');
+    this.productsModel = new ProductsModel();
 
     this.init();
   }
 
-  async init() {
+  init() {
+    this.initialRender();
+
+    this.setListeners();
+  }
+
+  async initialRender() {
     try {
       const products = await getProducts();
 
       this.productsView.render(products);
     } catch (error) {
-      console.error('Deu ruim', error);
+      console.error(error);
     }
+  }
+
+  setListeners() {
+    document.querySelector('#search-field').addEventListener('input', this.handleSearchFilter.bind(this));
+
+    document.querySelector('#category-field').addEventListener('change', this.handleCategoryFilter.bind(this));
+
+    document.querySelector('#sort-field').addEventListener('change', this.handleSort.bind(this));
+  }
+
+  handleSearchFilter({target}) {
+    const search = target.value.trim();
+
+    if(search.length === 0 || search.length > 2) {
+      this.applyFilters({search});
+    }
+  }
+
+  handleCategoryFilter({target}) {
+    this.applyFilters({category : target.value});
+  }
+
+  handleSort({target}) {
+    this.applyFilters({sortCriteria: target.value})
+  }
+
+  async applyFilters({
+    search = this.productsModel.filters.search ,
+    category = this.productsModel.filters.category,
+    sortCriteria = this.productsModel.filters.sortCriteria
+  }) {
+    this.productsModel.filters.search = search;
+    this.productsModel.filters.category = category;
+    this.productsModel.filters.sortCriteria = sortCriteria;
+
+    const filters = this.productsModel.buildFilters();
+    const products = await getProducts(filters);
+
+    this.productsView.render(products);
   }
 }
--- src/js/models/Products.js
+++ src/js/models/Products.js
@@ -1,7 +1,50 @@
-import ProductsController from '../controllers/Products.js';
-
 export default class ProductsModel {
-  constructor() {
+  filters = {
+    search: '',
+    category: 'all',
+    sortCriteria: '',
+  }
 
+  filterBySearch() {
+    if(this.filters.search === ''){
+      return {};
+    }
+
+    return {
+    'name_like': this.filters.search,
+    };
+  }
+
+  filterByCategory() {
+    if(this.filters.category === 'all') {
+      return {};
+    }
+
+    return {
+      category: this.filters.category,
+    }
+  }
+
+  sortByPrice() {
+    if(this.filters.sortCriteria === 'select') {
+      return {};
+    }
+
+    return {
+      '_sort': 'price',
+      '_order': this.filters.sortCriteria,
+    }
+  }
+
+  buildFilters() {
+    const filterBySearch = this.filterBySearch();
+    const filterByCategory = this.filterByCategory();
+    const sortCriteria = this.sortByPrice();
+
+    return new URLSearchParams({
+      ...filterBySearch,
+      ...filterByCategory,
+      ...sortCriteria,
+    }).toString();
   }
 }
